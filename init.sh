e89f0ad0d9f7ea3985e3bb7bde3cb60534f09a5b
robnagler <github@q33.us>
2016-04-19 01:58:56 +0000

    proxy_auth_token had newlines so needed to run "tr" on it.

--- init.sh
+++ init.sh
@@ -3,13 +3,17 @@
 # Run once
 #
 f=srv/pillar/apa20b.sls
+rand_base64() {
+    openssl rand -base64 "$1" | tr -d \\n
+}
+
 if [[ ! -f $f ]]; then
     # tornado.web._create_signature_v2 uses sha256 so need 256 bits max (32 bytes):
     # https://tools.ietf.org/html/rfc4868#section-2.1.1
-    cookie_secret=$(openssl rand -base64 32)
-    # This is passed around verbatim so just a long key that can fit
+    cookie_secret=$(rand_base64 32)
+    # This is passed around verbatim so just a long key (not decoded) that can fit
     # In the "Authorization: token $proxy_auth_token" header.
-    proxy_auth_token=$(openssl rand -base64 128)
+    proxy_auth_token=$(rand_base64 64)
     echo -n 'admin_user: '
     read admin_user
     cat <<EOF > "$f"
