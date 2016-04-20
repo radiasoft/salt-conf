dcaba9ebf7f8eee5fb6a15c01ccaba0e06893d00
robnagler <github@q33.us>
2016-04-20 21:41:50 +0000

    turn off local auth

--- srv/salt/jupyterhub/jupyterhub_config.py
+++ srv/salt/jupyterhub/jupyterhub_config.py
@@ -5,7 +5,7 @@ import base64
 c.JupyterHub.cookie_secret = base64.b64decode('{{ pillar.jupyterhub.cookie_secret }}')
 c.JupyterHub.proxy_auth_token = '{{ pillar.jupyterhub.proxy_auth_token }}'
 # Allow both local and GitHub users; Useful for bootstrap
-c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
+c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
 c.GitHubOAuthenticator.oauth_callback_url = 'https://jupyter.radiasoft.org/hub/oauth_callback'
 c.GitHubOAuthenticator.client_id = '{{ pillar.jupyterhub.github_client_id }}'
 c.GitHubOAuthenticator.client_secret = '{{ pillar.jupyterhub.github_client_secret }}'
