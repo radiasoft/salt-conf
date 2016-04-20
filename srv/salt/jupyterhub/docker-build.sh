9d8f90f3aab1cd4ba5fed1e689cdc5addf6fd9b7
robnagler <github@q33.us>
2016-04-20 15:48:03 +0000

    all output must be stderr except the "changed" line

--- srv/salt/jupyterhub/docker-build.sh
+++ srv/salt/jupyterhub/docker-build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -e
-export TMPDIR=/tmp/docker-build-$RANDOM
-exit_trap() {
-    cd /
-    rm -rf "$TMPDIR"
-}
-trap exit_trap EXIT
-mkdir "$TMPDIR"
-cd "$TMPDIR"
-cat >> build <<EOF
+(
+    set -e
+    export TMPDIR=/tmp/docker-build-$RANDOM
+    exit_trap() {
+        cd /
+        rm -rf "$TMPDIR"
+    }
+    trap exit_trap EXIT
+    mkdir "$TMPDIR"
+    cd "$TMPDIR"
+    cat >> build <<EOF
 user={{ pillar.jupyterhub.admin_user }}
 id=$(id -u {{ pillar.jupyterhub.admin_user }})
 groupadd -g "$id" "$user"
@@ -19,13 +20,14 @@ rm -rf ~/.cache
 cd /
 rm -rf /build
 EOF
-cat >> Dockerfile <<'EOF'
+    cat >> Dockerfile <<'EOF'
 FROM jupyter/jupyterhub
 ADD . /build
 RUN /build/build
 EOF
-tag=jupyter:$(date -u +%Y%m%d.%H%M%S)
-docker build -t jupyterhub .
-docker tag jupyterhub:latest "$tag"
-docker tag -f jupyterhub:latest "jupyter:{{ pillar.pykern_pkconfig_channel }}"
+    tag=jupyter:$(date -u +%Y%m%d.%H%M%S)
+    docker build -t jupyterhub .
+    docker tag jupyterhub:latest "$tag"
+    docker tag -f jupyterhub:latest "jupyter:{{ pillar.pykern_pkconfig_channel }}"
+) 1>&2
 echo "changed=yes comment='Build: $tag; {{ pillar.pykern_pkconfig_channel }}'"
