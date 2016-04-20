f1ce94f9e08454bc0785387fb8a8c2cbb407c424
robnagler <github@q33.us>
2016-04-20 21:40:24 +0000

    need to decode the bytestring for the cookie secret

--- srv/salt/jupyterhub/jupyterhub_config.py
+++ srv/salt/jupyterhub/jupyterhub_config.py
@@ -1,7 +1,8 @@
 c.Authenticator.admin_users = {'{{ pillar.jupyterhub.admin_user }}',}
 c.JupyterHub.confirm_no_ssl = True
 c.JupyterHub.ip = '0.0.0.0'
-c.JupyterHub.cookie_secret = '{{ pillar.jupyterhub.cookie_secret }}'
+import base64
+c.JupyterHub.cookie_secret = base64.b64decode('{{ pillar.jupyterhub.cookie_secret }}')
 c.JupyterHub.proxy_auth_token = '{{ pillar.jupyterhub.proxy_auth_token }}'
 # Allow both local and GitHub users; Useful for bootstrap
 c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
