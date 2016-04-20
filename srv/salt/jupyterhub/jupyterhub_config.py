d514558e6f229abbd34392bf34235db8ebf1b0d9
robnagler <github@q33.us>
2016-04-20 15:35:14 +0000

    allow local users too

--- srv/salt/jupyterhub/jupyterhub_config.py
+++ srv/salt/jupyterhub/jupyterhub_config.py
@@ -3,7 +3,8 @@ c.JupyterHub.confirm_no_ssl = True
 c.JupyterHub.ip = '0.0.0.0'
 c.JupyterHub.cookie_secret_file = '{{ zz.guest_cookie }}'
 c.JupyterHub.proxy_auth_token = '{{ pillar.jupyterhub.proxy_auth_token }}'
-c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+# Allow both local and GitHub users; Useful for bootstrap
+c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
 c.GitHubOAuthenticator.oauth_callback_url = 'https://jupyter.radiasoft.org/hub/oauth_callback'
 c.GitHubOAuthenticator.client_id = '{{ pillar.jupyterhub.github_client_id }}'
 c.GitHubOAuthenticator.client_secret = '{{ pillar.jupyterhub.github_client_secret }}'
